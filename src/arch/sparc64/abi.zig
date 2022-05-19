99da6788e1998cacef3755800a4a3e8f1bc8fefb
Jakub Konka <kubkon@jakubkonka.com>
2022-05-20 00:04:56 +0200

    regalloc: temporarily nerf back to raw ints until stage2 catches up

--- src/arch/sparc64/abi.zig
+++ src/arch/sparc64/abi.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const bits = @import("bits.zig");
 const Register = bits.Register;
 const RegisterManagerFn = @import("../../register_manager.zig").RegisterManager;
@@ -42,12 +43,5 @@ pub const RegisterManager = RegisterManagerFn(@import("CodeGen.zig"), Register, 
 // Register classes
 const RegisterBitSet = RegisterManager.RegisterBitSet;
 pub const RegisterClass = struct {
-    pub const gp: RegisterBitSet = blk: {
-        var set = RegisterBitSet.initEmpty();
-        set.setRangeValue(.{
-            .start = 0,
-            .end = allocatable_regs.len,
-        }, true);
-        break :blk set;
-    };
+    pub const gp: RegisterBitSet = std.math.maxInt(RegisterBitSet);
 };
